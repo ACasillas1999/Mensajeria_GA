42cd4000e94dbfcbd5c40e82c73327b0e91e7ed1
ACasillas1999 <acasillas.work@gmail.com>
2025-12-31 10:49:42 -0600

    tema claro oscuro cambiar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  darkMode: 'class', // Enable class-based dark mode
   theme: { extend: {} },
   plugins: [],
 };
